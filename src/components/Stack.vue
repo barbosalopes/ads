1f17ad55ec7f76552099939a5af2fa18b6433adf
barbosalopes <barbosalopesdeveloper@gmail.com>
2019-02-09 15:55:45 -0200

    insert update on enter logic and presentation as stack of data

--- src/components/Stack.vue
+++ src/components/Stack.vue
@@ -8,7 +8,7 @@
           </v-flex>
         </v-layout>
         <v-layout>
-          <v-flex xs6 md4>
+          <v-flex xs8 md4>
             <v-text-field
               v-model="item.value"
               :counter="40"
@@ -16,19 +16,16 @@
               required
             ></v-text-field>
           </v-flex>
+          <v-flex xs1 md4>
+            <v-btn color="#42f48c" @click='addItem(item.value).then(() => updateStack());'>+</v-btn>
+          </v-flex>
         </v-layout>
-        <v-layout v-for="item in stack" :key="item.id">
+        <v-layout v-for="item in stack.slice().reverse()" :key="item.id">
           <v-flex xs6 md4>
-            <v-text-field label="Value" v-model="item.value"></v-text-field>
+            <v-text-field label="Value" v-model="item.value" readonly></v-text-field>
           </v-flex>
-          <v-flex xs4 md4>
-            <v-btn color="#42f48c" @click='removeItem(item.id)'>X</v-btn>
-          </v-flex>
-        </v-layout>
-
-        <v-layout>
-          <v-flex xs12 md4>
-            <v-btn color="#42f48c" @click='addItem(item.value)'>Add Item</v-btn>
+          <v-flex xs4 md4 v-if="item.id == stack.length - 1">
+            <v-btn color="#42f48c" @click='removeItem().then(() => updateStack());'>X</v-btn>
           </v-flex>
         </v-layout>
       </v-container>
@@ -42,34 +39,29 @@ import {Item} from '../services/Item';
 
 export default Vue.extend({
     name: 'Form',
-    created: function() {
+    created() {
         this.updateStack();
-    },
-    props: {
-        stack: {
-            type: Array,
-            default: () => [],
-        },
     },
     data: () => ({
         item: {
             id: 1,
             value: '',
-        }
+        },
+        stack: [],
     }),
     methods: {
-        addItem(value) {
-            this.$http.post('https://localhost:44349/api/stack', value)
-        },
-        removeItem(id: number) {
-            
+        addItem(value: string) {
+            return this.$http.post('https://localhost:44349/api/stack/', {id: '0', value});
         },
         updateStack() {
             this.$http.get(
-              'https://localhost:44349/api/stack'
+              'https://localhost:44349/api/stack',
             ).then((response) => {
-                this.$props.stack = response.data;
+                this.$data.stack = response.data;
             });
+        },
+        removeItem() {
+            return this.$http.delete('https://localhost:44349/api/stack/');
         },
     },
 });
